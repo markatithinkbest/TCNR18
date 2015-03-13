953f3696871c136e25578c1c7ee90418cfc70089
Mark Chen <mark@ithinkbest.com>
2015-03-14 00:20:11 +0800

    replace dummy data to ArrayAdapter

--- app/src/main/java/com/ithinkbest/tcnr18/MainActivity.java
+++ app/src/main/java/com/ithinkbest/tcnr18/MainActivity.java
@@ -336,8 +336,14 @@ public class MainActivity extends ActionBarActivity
       //
             Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                    R.array.planets_array, android.R.layout.simple_spinner_item);
+//            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+//                    R.array.planets_array, android.R.layout.simple_spinner_item);
+
+//            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+//                    R.array.planets_array, android.R.layout.simple_spinner_item);
+            String[] memberList={"AAA","BBB","CCC"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),  android.R.layout.simple_spinner_item, memberList);
+
 // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
